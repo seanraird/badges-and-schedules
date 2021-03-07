78f6a145061030ce7e28fa5999f1b32ed41c9017
Sean Aird <seanraird@yahoo.com>
2021-03-07 15:36:39 -0500

    commit tree 35c231335e54941821851600952befcb7cb47a0a

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+
+def badge_maker(name)
+    name = "Arel"
+    return ("Hello, my name is #{name}.")
+end
+
+def batch_badge_creator(names)
+  names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]  
+    names.each do |name|
+        puts "Hello, my name is #{name}."
+    end
+end
